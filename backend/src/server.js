0a63e60290f6aa6b612ed2076fdccd0548c3e8a3
montasirrahman <montasirrahmanbd@gmail.com>
2024-01-02 11:39:51 +0600

    2/1/2024

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-
-// const app = express();
-// dotenv.config();
-// const PORT = process.env.PORT;
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port http://localhost:${PORT}`);
-// });
-
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
